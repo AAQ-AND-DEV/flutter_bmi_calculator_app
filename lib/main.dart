683e70c765d086a50c7eaf0a540ea2af4c0f362f
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-04-06 18:08:29 -0700

    renamed to specify purpose: BMI design style changes added prognosis TV and method

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'ui/home.dart';
 void main(){
   runApp(new MaterialApp(
     title: "Body Mass Index Calculator",
-    home: new Home(),
+    home: new Bmi(),
   ));
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
+class Bmi extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return new HomeState();
+    return new BmiState();
   }
 }
 
-class HomeState extends State<Home> {
-  final TextEditingController _heightControllerFeet = new TextEditingController();
-  final TextEditingController _heightControllerInches = new TextEditingController();
+class BmiState extends State<Bmi> {
+  final TextEditingController _heightControllerFeet =
+      new TextEditingController();
+  final TextEditingController _heightControllerInches =
+      new TextEditingController();
   final TextEditingController _weightController = new TextEditingController();
   final TextEditingController _ageController = new TextEditingController();
   double _bmi = 0.0;
   String _bmiMsg = "";
+  String _overweightMsg = "";
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +28,6 @@ class HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.pink,
       ),
-      backgroundColor: Colors.pinkAccent,
       body: new Container(
           alignment: Alignment.topCenter,
           child: new ListView(
@@ -33,17 +35,18 @@ class HomeState extends State<Home> {
             children: <Widget>[
               new Image.asset(
                 "images/bmilogo.png",
-                height: 150.0,
+                height: 80.0,
                 width: 150.0,
               ),
               Container(
                 margin: const EdgeInsets.all(3.0),
                 alignment: Alignment.center,
+                color: Colors.pinkAccent[100],
                 child: new Column(
                   children: <Widget>[
                     new TextField(
                       controller: _ageController,
-                      keyboardType: TextInputType.text,
+                      keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: "Age",
                         hintText: "in years",
@@ -74,57 +77,104 @@ class HomeState extends State<Home> {
                       decoration: new InputDecoration(
                         labelText: "Weight",
                         hintText: "Weight in lbs",
-                        icon: Icon(Icons.linear_scale),
+                        icon: Icon(Icons.insert_chart),
                       ),
                     ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new RaisedButton(
-                    child: new Text("calculate"),
-                    onPressed: (){
-                  setState(() {
-                    _bmi = _calcBMI(_weightController.text,_heightControllerFeet.text, _heightControllerInches.text);
-                    _bmiMsg = "your bmi is $_bmi";
-                  });}),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: new Text( _weightController.text.isEmpty||(_heightControllerFeet.text.isEmpty && _heightControllerInches.text.isEmpty)? "Please enter weight and height" :"$_bmiMsg",
-                  style: new TextStyle(
-                    color: Colors.brown,
-                    fontSize: 19.4,
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: new RaisedButton(
+                            child: new Text("calculate"),
+                            color: Colors.pink[800],
+                            textColor: Colors.white,
+                            onPressed: () {
+                              setState(() {
+                                _bmi = _calcBMI(
+                                    _weightController.text,
+                                    _heightControllerFeet.text,
+                                    _heightControllerInches.text);
+                                _bmiMsg =
+                                    "your bmi is ${_bmi.toStringAsFixed(2)}";
+                                _givePrognosis(_bmi);
+                              });
+                            }),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: new Text(
+                        _weightController.text.isEmpty ||
+                                (_heightControllerFeet.text.isEmpty &&
+                                    _heightControllerInches.text.isEmpty)
+                            ? "Please enter weight and height"
+                            : "$_bmiMsg",
+                        style: new TextStyle(
+                          color: Colors.brown,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 19.4,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: new Text(
+                        "Prognosis: $_overweightMsg",
+                        style: new TextStyle(
+                          color: Colors.red[900],
+                          fontWeight: FontWeight.w700,
+                          fontSize: 19.4,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-
-                  ],
-              ),
-              ),
-
             ],
           )),
     );
   }
-  double _calcBMI(String weight, String heightFeet, String heightInches){
+
+  double _calcBMI(String weight, String heightFeet, String heightInches) {
     double res = 0.0;
     int lbs = 0;
-    int ht = 0;
-    if (int.parse(weight).toString().isNotEmpty && int.parse(weight)>0){
+    int heightInInches = 0;
+    if (int.parse(weight).toString().isNotEmpty && int.parse(weight) > 0) {
       lbs = int.parse(weight);
-    } else {print("enter weight");}
-    if (int.parse(heightFeet).toString().isNotEmpty && int.parse(heightFeet)>1){
-      ht = int.parse(heightFeet)*12;
+    } else {
+      print("enter weight");
     }
-    if (int.parse(heightInches).toString().isNotEmpty && int.parse(heightInches)>0){
-      ht += int.parse(heightInches);
+    if (int.parse(heightFeet).toString().isNotEmpty &&
+        int.parse(heightFeet) > 1) {
+      heightInInches = int.parse(heightFeet) * 12;
     }
-    if (int.parse(heightInches).toString().isNotEmpty || (int.parse(heightFeet).toString().isNotEmpty) && int.parse(weight).toString().isNotEmpty){
-      res = lbs/(ht*ht)*703;
-    } else{
+    if (int.parse(heightInches).toString().isNotEmpty &&
+        int.parse(heightInches) > 0) {
+      heightInInches += int.parse(heightInches);
+    }
+    if (int.parse(heightInches).toString().isNotEmpty ||
+        (int.parse(heightFeet).toString().isNotEmpty) &&
+            int.parse(weight).toString().isNotEmpty) {
+      res = lbs / (heightInInches * heightInInches) * 703;
+    } else {
       print("enter both a weight and a height");
     }
     return res;
+  }
 
+  void _givePrognosis(double bmi) {
+    if (bmi < 18.5) {
+      _overweightMsg = "underweight";
+      print(_overweightMsg);
+    } else if (bmi >= 18.5 && bmi < 25) {
+      _overweightMsg = "normal";
+      print(_overweightMsg);
+    } else if (bmi >= 25.0 && bmi < 30) {
+      _overweightMsg = "overweight";
+      print(_overweightMsg);
+    } else {
+      _overweightMsg = "obese";
+      print(_overweightMsg);
+    }
   }
 }
